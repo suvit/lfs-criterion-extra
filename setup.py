0e3f521d3b6d406a12646ae0b590eb874e9771d9
Victor Safronovich <vsafronovich@gmail.com>
2012-08-22 13:59:55 +0600

    remove another project name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -
 #
-# This file is part of speedydeploy released under the MIT license. 
+# This file is part of lfs-criterion-extra released under the MIT license. 
 # See the NOTICE for more information.
 
 import os
